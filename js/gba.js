0219ab4beec1ddf28213ed3739b54850514b5181
MKatwood <mckenzie@mkatwood.com>
2018-04-24 13:56:02 -0400

    Attempt #6

--- js/gba.js
+++ js/gba.js
@@ -118,8 +118,6 @@ GameBoyAdvance.prototype.hasRom = function() {
 
 GameBoyAdvance.prototype.loadRomFromFile = function(romFile, callback) {
 	console.log(romFile);
-	var x = new File("resources/Hookslide.gba");
-	console.log(x);
 	var reader = new FileReader();
 	var self = this;
 	reader.onload = function(e) {
